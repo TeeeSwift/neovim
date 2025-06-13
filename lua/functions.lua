cab7457a65ce31e9917770b7042836cd04b5ce86
teeeswift <chung.taylor1@gmail.com>
2025-06-13 09:47:28 +0900

    Improve Lazy installation/loading

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,5 +1,1 @@
-vim.cmd[[command! RankAndFile lua vim.fn.setreg('+', vim.fn.expand('%')..':'..vim.fn.line('.'))]]
-vim.cmd([[
-  command! MERM call system("~/scripts/mermaid_to_clipboard.sh")
-]])
-
+vim.cmd [[command! RankAndFile lua vim.fn.setreg('+', vim.fn.expand('%')..':'..vim.fn.line('.'))]]
