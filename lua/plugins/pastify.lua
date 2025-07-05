71a78e5de44f332bd28cdf74725585213ed2f1d5
teeeswift <chung.taylor1@gmail.com>
2025-07-05 21:21:07 +0900

    change pastify default md

--- lua/plugins/pastify.lua
+++ lua/plugins/pastify.lua
@@ -1,20 +1,21 @@
 return {
   'TobinPalmer/pastify.nvim',
   cmd = { 'Pastify', 'PastifyAfter' },
-  event = { 'BufReadPost' },   -- Load after the buffer is read, I like to be able to paste right away
+  event = { 'BufReadPost' }, -- Load after the buffer is read, I like to be able to paste right away
   config = function()
     require('pastify').setup({
       opts = {
-        absolute_path = false,                -- use absolute or relative path to the working directory
-        apikey = '',                          -- Api key, required for online saving
-        local_path = '/assets/imgs/',         -- The path to put local files in, ex ~/Projects/<name>/assets/images/<imgname>.png
-        save = 'local',                       -- Either 'local' or 'online' or 'local_file'
+        absolute_path = false,        -- use absolute or relative path to the working directory
+        apikey = '',                  -- Api key, required for online saving
+        local_path = '/assets/imgs/', -- The path to put local files in, ex ~/Projects/<name>/assets/images/<imgname>.png
+        save = 'local',               -- Either 'local' or 'online' or 'local_file'
         filename = function() return vim.fn.expand("%:t:r") .. '_' .. os.date('%Y-%m-%d_%H-%M-%S') end,
-        default_ft = 'markdown',              -- Default filetype to use
+        default_ft = 'markdown',      -- Default filetype to use
       },
-      ft = {                                  -- Custom snippets for different filetypes, will replace $IMG$ with the image url
+      ft = {                          -- Custom snippets for different filetypes, will replace $IMG$ with the image url
         html = '<img src="$IMG$" alt="">',
-        markdown = '![]($IMG$)',
+        -- markdown = '![]($IMG$)',
+        markdown = '<img src="$IMG$">',
         tex = [[\includegraphics[width=\linewidth]{$IMG$}]],
         css = 'background-image: url("$IMG$");',
         js = 'const img = new Image(); img.src = "$IMG$";',
