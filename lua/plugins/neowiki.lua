971eb2780ec9418bc4f96c8bdcbb1741cefb5d55
teeeswift <chung.taylor1@gmail.com>
2025-07-07 17:07:31 +0900

    add neowiki

--- lua/plugins/neowiki.lua
+++ lua/plugins/neowiki.lua
@@ -2,13 +2,25 @@ return {
   "echaya/neowiki.nvim",
   config = function()
     local neowiki = require 'neowiki'
+
     vim.keymap.set("n", "<leader>zk", function()
       neowiki.open_wiki_floating()
     end, { desc = "Open Work Wiki" })
 
+    vim.api.nvim_create_user_command(
+      "OpenWiki", -- The name of your command (e.g., :HelloNvim)
+      function()
+        neowiki.open_wiki_new_tab()
+      end,
+      {} -- Optional attributes table (more on this below)
+    )
+
+
+
     neowiki.setup({
       wiki_dirs = {
-        { name = "wiki", path = "~/Documents/wiki" },
+        { name = "spp", path = "~/Documents/wiki/spp" },
+        { name = "e1",  path = "~/Documents/wiki/e1" },
       },
       keymaps = {
         toggle_task = "<C-k>",
